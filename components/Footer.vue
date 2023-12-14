e6034749f04a7ab46db7e3675b69f452f3f79fd9
mystic <mystic@izmystic.dev>
2023-12-14 16:18:07 -0500

    init: vue remake

---
+++ components/Footer.vue
@@ -0,0 +1,22 @@
+<script setup>
+const links = [
+  {
+    label: "Nuxt UI",
+    to: "https://ui.nuxt.com/",
+  },
+  {
+    label: "Nuxt Docs",
+    to: "https://nuxt.com",
+  },
+  {
+    label: "Nuxt Studio",
+    to: "https://nuxt.studio",
+  },
+];
+</script>
+
+<template>
+  <UFooter>
+    <template #center> Copyright © {{ new Date().getFullYear() }} </template>
+  </UFooter>
+</template>
---
+++ components/Header.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import type { NavItem } from "@nuxt/content/dist/runtime/types";
+
+const navigation = inject<Ref<NavItem[]>>("navigation", ref([]));
+
+const links = [
+  {
+    label: "Home",
+    to: "#home",
+  },
+  {
+    label: "About",
+    to: "#about",
+  },
+  {
+    label: "Services",
+    to: "#services",
+  },
+  {
+    label: "Portfolio",
+    to: "#portfolio",
+  },
+];
+</script>
+
+<template>
+  <UHeader :links="links">
+    <template #left> </template>
+
+    <template #right>
+      <UColorModeButton />
+    </template>
+
+    <template #panel>
+      <UNavigationTree :links="mapContentNavigation(navigation)" />
+    </template>
+  </UHeader>
+</template>
