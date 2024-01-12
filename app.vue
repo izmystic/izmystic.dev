4d428ec21a99f52106729d6567ff86c6ff10b0d6
mystic <mystic@izmystic.dev>
2024-01-12 15:25:22 -0500

    yeppers

---
+++ app.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+useHead({
+  meta: [{ name: "viewport", content: "width=device-width, initial-scale=1" }],
+  link: [{ rel: "icon", href: "https://avatars.githubusercontent.com/u/72127044?v=4" }],
+  htmlAttrs: {
+    lang: "en",
+  },
+});
+
+useSeoMeta({
+  ogSiteName: "izmystic",
+  twitterCard: "summary_large_image",
+});
+</script>
+
+<template>
+  <UMain>
+    <NuxtPage />
+  </UMain>
+
+  <Footer />
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,4 @@
 <script setup lang="ts">
-import Header from "../components/Header.vue";
-import Footer from "../components/Footer.vue";
-
 const links = [
   {
     label: "GitHub",
@@ -28,32 +25,11 @@ const links = [
 </script>
 
 <template>
-  <Header />
-  <UMain>
-    <NuxtLayout>
-      <UPage>
-        <UPageBody prose>
-          <ULandingSection id="home" title="izmystic" description="Website Designer | Graphic Designer" :links="links" align="left">
-            <img class="rounded-full" src="https://avatars.githubusercontent.com/u/72127044?v=4" />
-          </ULandingSection>
-        </UPageBody>
-
-        <UPageBody prose>
-          <ULandingSection id="services" title="Services"> </ULandingSection>
-        </UPageBody>
-
-        <UPageBody prose>
-          <ULandingSection id="portfolio" title="Portfolio">
-            <ULandingGrid>
-              <ULandingCard class="col-span-6 row-span-4" title="Card Site" description="My initial developer card website"> <img src="https://github.com/izmystic/izmystic.dev/assets/72127044/738386f6-9fa2-44a1-afe6-90bce1ad1ef4" class="w-full rounded-md" /></ULandingCard>
-              <ULandingCard class="col-span-6 row-span-4" title="Project #2" description="Quick description for Project #2"> <img src="https://picsum.photos/610/360" class="w-full rounded-md" /></ULandingCard>
-              <ULandingCard class="col-span-6 row-span-4" title="Project #3" description="Quick description for Project #3"> <img src="https://picsum.photos/620/360" class="w-full rounded-md" /></ULandingCard>
-              <ULandingCard class="col-span-6 row-span-4" title="Project #4" description="Quick description for Project #4"> <img src="https://picsum.photos/640/360" class="w-full rounded-md" /></ULandingCard>
-            </ULandingGrid>
-          </ULandingSection>
-        </UPageBody>
-      </UPage>
-    </NuxtLayout>
-  </UMain>
-  <Footer />
+  <ULandingSection>
+    <UPageHeader id="home" title="izmystic" description="Website Designer | Graphic Designer" :links="links" align="left" class="!border-opacity-0">
+      <template #icon>
+        <img src="https://avatars.githubusercontent.com/u/72127044?v=4" class="w-52 h-52 object-cover rounded-full" />
+      </template>
+    </UPageHeader>
+  </ULandingSection>
 </template>
